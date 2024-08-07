8b73d25b362e96a8ffd02fbad13e312f483bd854
ShyRobin <shy_robin@163.com>
2024-08-08 01:58:00 +0800

    feat: popup init

---
+++ src/components/ConfigCard.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="config-card">
+    <div class="config-card__header">配置</div>
+    <div class="config-card__body">
+      <div class="config-card__item">
+        <div class="config-card__item-label">单窗口标签最大显示数量</div>
+        <n-input-number
+          v-model:value="maxTabsCount"
+          clearable
+          size="small"
+          :min="1"
+        />
+      </div>
+      <div class="config-card__item">
+        <div class="config-card__item-label">标签整理规则</div>
+        <n-select v-model:value="rule" size="small" :options="ruleOptions" />
+      </div>
+      <div class="config-card__item">
+        <div class="config-card__item-label">分组文案</div>
+        <n-input v-model:value="groupLabel" clearable size="small" />
+      </div>
+      <div class="config-card__item">
+        <div class="config-card__item-label">分组颜色</div>
+        <n-select
+          v-model:value="groupColor"
+          size="small"
+          :options="groupColorOptions"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { NInputNumber, NInput, NSelect } from "naive-ui";
+
+const maxTabsCount = ref(7);
+const groupLabel = ref("MORE");
+const groupColor = ref("blue");
+const rule = ref("lastAccessed");
+
+const ruleOptions = [
+  {
+    label: "最近访问时间",
+    value: "lastAccessed",
+  },
+];
+const groupColorOptions = [
+  {
+    label: "灰",
+    value: "grey",
+  },
+  {
+    label: "蓝",
+    value: "blue",
+  },
+  {
+    label: "红",
+    value: "red",
+  },
+  {
+    label: "黄",
+    value: "yellow",
+  },
+  {
+    label: "green",
+    value: "绿",
+  },
+  {
+    label: "粉",
+    value: "pink",
+  },
+  {
+    label: "紫",
+    value: "purple",
+  },
+  {
+    label: "青",
+    value: "cyan",
+  },
+  {
+    label: "橙",
+    value: "orange",
+  },
+];
+</script>
+
+<style lang="less" scoped>
+.config-card {
+  width: 100%;
+  color: #000;
+  &__header {
+    color: #fff;
+    background-color: #646cff;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    border-bottom: none;
+    font-size: 14px;
+    padding: 4px;
+  }
+  &__body {
+    border: 1px solid #ddd;
+    border-top: none;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    padding: 8px;
+  }
+  &__item {
+    text-align: start;
+    margin-bottom: 8px;
+    &-label {
+      color: #747bff;
+      font-size: 12px;
+      margin-bottom: 8px;
+    }
+  }
+}
+</style>
